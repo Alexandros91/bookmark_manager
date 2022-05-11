69a2c2aedc4103a970128a5a427e0b301dea500d
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-05-11 17:09:59 +0100

    Updated spec

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -9,10 +9,8 @@ feature 'Viewing bookmarks' do
     connection.exec("INSERT INTO bookmarks (title, url) VALUES ('Google', 'http://www.google.com/');")
 
     visit('/bookmarks')
-    
-    # save_and_open_page
 
-    # expect(page).to have_content "www.google.com/"
+    expect(page).to have_content "www.google.com/"
     expect(page).to have_content "www.makersacademy.com/"
     expect(page).to have_content "http://www.destroyallsoftware.com/"
     expect(page).to have_content "Google"
